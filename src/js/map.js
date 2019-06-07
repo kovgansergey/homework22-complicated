b6bb10182410b72e3cb0a9e75fabe67880af761c
kovgansergey <skydiver.88@mail.ru>
2019-06-07 22:11:15 +0700

    добавил маркер на карту и заблокировал скроллинг мышью

--- src/js/map.js
+++ src/js/map.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
         function init(){ 
             var myMap = new ymaps.Map("footerMap", {
                 center: [55.611409, 37.201122],
-                zoom: 16
+                zoom: 12,
             }); 
+            var myPlacemark = new ymaps.Placemark([55.611409, 37.201122], null, {
+                preset: 'islands#yellowIcon',
+            });
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom');
         }
 });
